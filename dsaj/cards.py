c772fb759f9e3006aa52a0f5dcabd79717735ecd
nandweza <allannandweza@gmail.com>
2022-07-16 09:45:25 +0300

    working with list

---
+++ dsaj/cards.py
@@ -0,0 +1,23 @@
+# cards[]: list containing cards
+# query: the number to search
+# position: index
+
+def locate_cards(cards, query):
+    position = 0
+    #print('cards:', cards)
+    #print('query:', query)
+
+    #the position has to be less than the len(cards) so we can access it.
+    while position < len(cards):
+        #print('position:', position)
+
+        if cards[position] == query:
+            return position
+        position += 1
+    return -1
+
+a = [9, 8, 7, 5, 4, 2, 1]
+b = 5
+
+c = locate_cards(a, b)
+print(c)
