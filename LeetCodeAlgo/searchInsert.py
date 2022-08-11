b5ba8a0a647b20206f1c439f5e71ee25f9123bdc
nandweza <allannandweza@gmail.com>
2022-08-11 19:09:24 +0300

    algorithms

--- LeetCodeAlgo/searchInsert.py
+++ LeetCodeAlgo/searchInsert.py
@@ -27,3 +27,14 @@ nums contains distinct values sorted in ascending order.
 -104 <= target <= 104
 """
 def searchInsert(nums: List[int], target: int) -> int:
+    lo, hi = 0, len(nums) - 1
+
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return lo
---
+++ swap.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+a = 10
+b = 30
+
+print(a)
+print(b)
+print(end="\n")
+
+temp = a
+a = b
+b = temp
+
+print(a)
+print(b)
