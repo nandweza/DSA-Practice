d73d4757e85f3c868a2125acfc91a52a46e83b5a
nandweza <allannandweza@gmail.com>
2022-10-12 09:01:59 +0300

    python array

---
+++ Data-structures/arrayPractice.py
@@ -0,0 +1,16 @@
+# 1. Create an array and traverse.
+# 2. Access individual elements thru indexes.
+# 3. Append any value to the array using append() method.
+# 4. Insert value in an array using insert() method.
+# 5. Extend python array using extend() method.
+# 6. Add items from list into array using fromlist() mtd.
+# 7. Remove any array element using remove() mtd.
+# 8. Remove last array element using pop() mtd.
+# 9. Fetch any element thru its index using index() mtd.
+# 10.Reverse a python array using reverse() mtd.
+# 11.Get array buffer information thru buffer_info() mtd.
+# 12.Check for number of occurrences of an element using count() mtd.
+# 13.Convert array to string using tostring() mtd.
+# 14.Convert array to a python list with same elements using tolist() mtd.
+# 15.Append a string to char array using fromstring() mtd.
+# 16.Slice elements from an array.
